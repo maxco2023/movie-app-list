5359db9db7fc62f897ef4f6eb1f44a1c00e9a4ec
maxco2023 <maxime.coquerelle@gmail.com>
2023-04-13 15:09:40 +0200

    modal

--- app/javascript/controllers/modal_controller.js
+++ app/javascript/controllers/modal_controller.js
@@ -4,61 +4,37 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
 
   connect() {
-    console.log("test connexion")
     // Get the modal
     var modal = document.getElementById("myModal");
-    var modal2 = document.getElementById("myModal2");
-    var modal3 = document.getElementById("myModal3");
-
 
     // Get the button that opens the modal
     var btn = document.getElementById("myBtn");
     var btn2 = document.getElementById("myBtn2");
     var btn3 = document.getElementById("myBtn3");
 
-
     // Get the <span> element that closes the modal
     var span = document.getElementsByClassName("close")[0];
-    var span2 = document.getElementsByClassName("close2")[0];
-    var span3 = document.getElementsByClassName("close3")[0];
-
 
     // When the user clicks on the button, open the modal
     btn.onclick = function() {
       modal.style.display = "block";
     }
     btn2.onclick = function() {
-      modal2.style.display = "block";
+      modal.style.display = "block";
     }
     btn3.onclick = function() {
-      modal3.style.display = "block";
+      modal.style.display = "block";
     }
 
     // When the user clicks on <span> (x), close the modal
     span.onclick = function() {
       modal.style.display = "none";
     }
-    span2.onclick = function() {
-      modal2.style.display = "none";
-    }
-    span3.onclick = function() {
-      modal3.style.display = "none";
-    }
 
     // When the user clicks anywhere outside of the modal, close it
     window.onclick = function(event) {
       if (event.target == modal) {
         modal.style.display = "none";
-      }
-    };
-    window.onclick = function(event) {
-      if (event.target == modal2) {
-        modal2.style.display = "none";
-      }
-    };
-    window.onclick = function(event) {
-      if (event.target == modal3) {
-        modal3.style.display = "none";
       }
     };
   }
